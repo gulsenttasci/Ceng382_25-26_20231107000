7d0aca5824572179c1779bb41ac3c27ebf9070da
Gulsen Tasci <gulsentasci47@gmail.com>
2026-03-07 18:44:50 +0300

    proje yapısı

---
+++ Ceng382_25-26_20231107000/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace Ceng382_25_26_20231107000.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
