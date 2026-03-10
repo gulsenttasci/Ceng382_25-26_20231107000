010d0e258b044557b74fe076f03fd173330c89a8
Gulsen Tasci <gulsentasci47@gmail.com>
2026-03-10 11:16:03 +0300

    Model: ProductCard class filled with features

---
+++ Models/ProductCard.cs
@@ -0,0 +1,25 @@
+ namespace Ceng382_25_26_202311070.Models;
+
+
+
+public class ProductCard
+
+{
+
+    public int Id { get; set; }
+
+    public string? Name { get; set; }
+
+    public int Midterm { get; set; }
+
+    public int Final { get; set; }
+
+    public string? Grade { get; set; }
+
+    public string? Category { get; set; }
+
+    public decimal Price { get; set; }
+
+    public string? Description { get; set; }
+
+}
